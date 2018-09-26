9ce916ad1360c37b7754254270e5311335156a51
klin <ironcarbide@gmail.com>
2018-09-26 11:07:36 -0700

    Slider now resizes the particle displayed.

--- src/VerticalSlider.js
+++ src/VerticalSlider.js
@@ -1,58 +1,58 @@
-import React, { Component } from 'react'
-import Slider from 'react-rangeslider'
-import 'react-rangeslider/lib/index.css'
-import './App.css';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import Slider from "react-rangeslider";
+import "react-rangeslider/lib/index.css";
+import "./App.css";
 
 class Vertical extends Component {
-  constructor (props, context) {
-    super(props, context)
+  constructor(props, context) {
+    super(props, context);
     this.state = {
-      value: 25,
-    }
+      value: 25
+    };
   }
 
-  handleChange = (value) => {
+  handleChange = value => {
     this.setState({
       value: value
-    })
-  }
+    });
+  };
 
-
-  render () {
-    const { value } = this.state
+  render() {
+    const { value } = this.state;
     return (
-        <div>
+      <div>
         <div className="App">
-        <header className="App-header">
-          <br></br>
-          <br></br>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">It's a particle!</h1>
-        </header>
-      </div>
-      
-        <div ClassName='App'>
-      <div className='slider orientation-reversed'>
-        <div className='slider-group'>
-          <div className='slider-vertical'>
-            <Slider
-              min={0}
-              max={100}
-              value={value}
-              orientation='vertical'
-              onChange={this.handleChange}
-            />
-            <div className='value'>{value}</div>
-          </div>
+          <header className="App-header">
+            <br />
+            <br />
+            <svg className="App-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 841.9 595.3">
+              <g fill="#647156">
+                <circle cx="420.9" cy="300" r={value} />
+              </g>
+            </svg>
+            <h1 className="App-title">It's a resizeable particle!</h1>
+          </header>
+        </div>
 
+        <div ClassName="App">
+          <div className="slider orientation-reversed">
+            <div className="slider-group">
+              <div className="slider-vertical">
+                <Slider
+                  min={1}
+                  max={200}
+                  value={value}
+                  orientation="vertical"
+                  onChange={this.handleChange}
+                />
+                <div className="value">{value}</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-      </div>
-
-    )
+    );
   }
 }
 
-export default Vertical
+export default Vertical;
